8cc29d82d524e247cf8539f381e75a6c3c1f4bbe
Haiya Patel <161632452+Haiya-P@users.noreply.github.com>
2026-02-28 21:43:41 -0500

    Moved the questionpop to a different file, added tasks array that is utilized by picktasks to randomly assign a task. Every checked task you have makes your flower bloom.

--- my-react-router-app/app/welcome/welcome.tsx
+++ my-react-router-app/app/welcome/welcome.tsx
@@ -1,24 +1,11 @@
 import userLogo from "./Sprout.webp";
 import { FlowerPop } from "./popupcomps/flowerpop";
+import { QuestionPop } from "./popupcomps/questionpop";
 
 export function Welcome() {
   return (
 
-    // This is the ? popup
-    <main className="relative flex items-center justify-center pt-16 pb-4">
-      <details className="fixed top-4 right-4 z-40">
-        <summary
-          aria-label="Toggle help popup"
-          className="flex h-10 w-10 cursor-pointer list-none items-center justify-center rounded-md border border-gray-300 bg-white text-xl font-semibold shadow-sm hover:bg-gray-50 [&::-webkit-details-marker]:hidden"
-        >
-          ❓
-        </summary>
-        <div className="absolute top-12 right-0 z-30 w-80 rounded-lg border border-gray-300 bg-white p-4 shadow-md">
-          <p className="text-sm text-gray-700">Welcome to `insert name here`! To grow a flower, complete tasks to help your sprout grow. After accumulating 3 points, your sprout will bloom into a flower! Pick your flower and put it in a flower field with a message for others to read!</p>
-        </div>
-      </details>
-
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="flex min-h-0 flex-1 flex-col items-center">
         <header className="flex flex-col items-center gap-9">
@@ -27,11 +14,10 @@ export function Welcome() {
           </div>
         </header>
       </div>
-
+        <QuestionPop />
       <div>
         <FlowerPop />
       </div>
-
     </main>
   );
 }
